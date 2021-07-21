dfec2e2703412ea293d9269ba4a99408f238cf3f
Amarjit-Madhumalararungeethayan <m.a.amarjit@gmail.com>
2021-07-21 17:40:46 +0530

    spiderTask2AppDev

--- app/src/main/java/com/example/minetime/Battleground.kt
+++ app/src/main/java/com/example/minetime/Battleground.kt
@@ -1,5 +1,6 @@
 package com.example.minetime
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
@@ -15,7 +16,6 @@ import androidx.core.content.ContextCompat
 var xCoordinates = mutableListOf<Int>(10)
 var yCoordinates = mutableListOf<Int>(10)
 
-var numberOfMines = 0
 var runCount = 0
 
 var xClick = 0
@@ -30,6 +30,9 @@ var x1Blue = 0f
 var x2Blue = 0f
 var y1Blue = 0f
 var y2Blue = 0f
+
+private lateinit var extraCanvas: Canvas
+private lateinit var extraBitmap: Bitmap
 
 class Battleground(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
@@ -68,9 +71,14 @@ class Battleground(context: Context, attrs: AttributeSet?) : View(context, attrs
 
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int)
     {
         super.onSizeChanged(width, height, oldwidth, oldheight)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(R.color.black)
     }
 
     var startX = 100f
@@ -79,6 +87,7 @@ class Battleground(context: Context, attrs: AttributeSet?) : View(context, attrs
     override fun onDraw(canvas: Canvas?) {
 
         super.onDraw(canvas)
+
         canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), bg)
 
         for (i in 1..9) {
@@ -91,15 +100,20 @@ class Battleground(context: Context, attrs: AttributeSet?) : View(context, attrs
         //grid starts at (160,160) and has a spacing of 100f
         // So the centre of a square would be of the format (210 + 100i),(210 + 100j) where i and j vary from 0 to 7
 
-        //canvas?.drawCircle(210f + (100 * a).toFloat(), 210f + (100 * b).toFloat(), 25f, bomb)
         for(i in 0..7){
             for(j in 0..7){
                 canvas?.drawRect(170f + (100*i).toFloat(), 170f + (100*j).toFloat(),250f + (100*i).toFloat(),250f + (100*j).toFloat(),disguise)
             }
-
         }//l,t,r,b
             canvas?.drawRect(x1Blue, y1Blue, x2Blue, y2Blue, blue)
             canvas?.drawRect(x1Red, y1Red, x2Red, y2Red, red)
+
+        extraCanvas.drawRect(x1Blue, y1Blue, x2Blue, y2Blue, blue)
+        extraCanvas.drawRect(x1Red, y1Red, x2Red, y2Red, red)
+
+        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+
+
     }
     fun chooseCoors(){
         for (i in 0..runCount) {
@@ -138,6 +152,7 @@ class Battleground(context: Context, attrs: AttributeSet?) : View(context, attrs
                    y1Red = (170 + (100*yCoordinates[i])).toFloat()
                    y2Red = y1Red + 80f
                    invalidate()
+                   runG = false
                }
                else{
                    Log.d("abc","Blank")
--- app/src/main/java/com/example/minetime/MainActivity.kt
+++ app/src/main/java/com/example/minetime/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.minetime
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.ActionBar
 import androidx.core.view.isVisible
 import com.example.minetime.databinding.ActivityMainBinding
+
+var Point = 0
+var runG = true
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,16 +21,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.button2.isEnabled = false
 
-        binding.button.setOnClickListener(){
+        binding.button.setOnClickListener() {
             binding.battleground.refresh()
             binding.battleground.chooseCoors()
             binding.button2.isEnabled = true
             binding.textView.text = ""
-
-
+            scoreRefresh()
         }
-        binding.button2.setOnClickListener(){
-            binding.textView.text = "[ (${xCoordinates[0]},${yCoordinates[0]}) , (${xCoordinates[1]},${yCoordinates[1]}) , (${xCoordinates[2]},${yCoordinates[2]}) , (${xCoordinates[3]},${yCoordinates[3]}) , (${xCoordinates[4]},${yCoordinates[4]}) , (${xCoordinates[5]},${yCoordinates[5]}) , (${xCoordinates[6]},${yCoordinates[6]}) ]"
+        binding.button2.setOnClickListener() {
+            binding.textView.text =
+                "[ (${xCoordinates[0]},${yCoordinates[0]}) , (${xCoordinates[1]},${yCoordinates[1]}) , (${xCoordinates[2]},${yCoordinates[2]}) , (${xCoordinates[3]},${yCoordinates[3]}) , (${xCoordinates[4]},${yCoordinates[4]}) , (${xCoordinates[5]},${yCoordinates[5]}) , (${xCoordinates[6]},${yCoordinates[6]}) ]"
             binding.button2.isEnabled = false
         }
 
@@ -33,6 +38,39 @@ class MainActivity : AppCompatActivity() {
         if (actionBar != null) {
             actionBar.hide()
         }
+
+
     }
 
+    private fun scoreRefresh() {
+        val countDown: CountDownTimer
+        countDown = object : CountDownTimer(10000000, 10) {
+            override fun onTick(millisecsToFinish: Long) {
+                binding.textView3.text = "Points -> ${Point}"
+
+                if(runG == false){
+                    binding.textView3.text = "GAME OVER"
+                    newGame()
+                }
+            }
+
+            override fun onFinish() {
+                scoreRefresh()
+            }
+        }
+        countDown.start()
+    }
+
+    private fun newGame() {
+        val countDown: CountDownTimer
+        countDown = object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisecsToFinish: Long) {
+                }
+
+            override fun onFinish() {
+            }
+        }
+        countDown.start()
+    }
 }
+
